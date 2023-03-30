a7783dbdbdc9083576aafeb4f71aab7e2436ab35
Aidin Menkovic <shungbay94@yahoo.de>
2023-03-30 14:29:12 +0200

    initial commit

---
+++ src/react.main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { AppComponent } from './components/app/component';
+
+import { register } from '@public-ui/components';
+import { defineCustomElements } from '@public-ui/components/dist/loader';
+import { ITZBund } from '@public-ui/themes';
+
+register(ITZBund, defineCustomElements)
+	.then(() => {
+		const htmlElement: HTMLElement | null = document.querySelector<HTMLDivElement>('div#app');
+		if (htmlElement instanceof HTMLElement) {
+			const root = createRoot(htmlElement);
+			root.render(<AppComponent />);
+		}
+	})
+	.catch(console.warn);
